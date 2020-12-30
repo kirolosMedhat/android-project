ace3d491e26cc36c270a106c1d634e5218d74843
PhiloAtef <50450445+PhiloAtef@users.noreply.github.com>
2020-12-30 20:00:16 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/tictactoe/HomeActivty.java
@@ -0,0 +1,41 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivty extends AppCompatActivity {
+
+    private Button SinglePlayer;
+    private Button Localmultiplayer;
+    private Button Wifimultiplayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_activty);
+        SinglePlayer=(Button) findViewById(R.id.Singleplayer);
+        SinglePlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+        Localmultiplayer=(Button) findViewById(R.id.Localbtt);
+        Localmultiplayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+        Wifimultiplayer=(Button) findViewById(R.id.Wifibtt);
+        SinglePlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+    }
+}
